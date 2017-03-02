ead6adb08b186f59d0698d452a7ca6c5cd911507
woutster <wapkooijman@gmail.com>
2017-03-02 12:30:35 +0100

    working picture taking and speech recognition

--- MediaUnderstanding/app/src/main/java/com/example/albert/mediaunderstanding/MainActivity.java
+++ MediaUnderstanding/app/src/main/java/com/example/albert/mediaunderstanding/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.albert.mediaunderstanding;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
@@ -15,17 +17,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     private final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView voiceInput;
-    private ImageView mImageView;
+    private static String root = null;
+    private static String imageFolderPath = null;
+    private String imageName = null;
+    private static Uri fileUri = null;
+    private static final int CAMERA_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +41,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openCamera(View view) {
+
+        ImageView imageView = (ImageView) findViewById(R.id.photo);
+
+        // fetching the root directory
+        root = Environment.getExternalStorageDirectory().toString()
+                + "/Your_Folder";
+
+        // Creating folders for Image
+        imageFolderPath = root + "/saved_images";
+        File imagesFolder = new File(imageFolderPath);
+        imagesFolder.mkdirs();
+
+        // Generating file name
+        SimpleDateFormat s = new SimpleDateFormat("ddMMyyyyhhmmss");
+        String timeStamp = s.format(new Date());
+        imageName = timeStamp + ".png";
+
+        // Creating image here
+        File image = new File(imageFolderPath, imageName);
+
+        fileUri = Uri.fromFile(image);
+        imageView.setTag(imageFolderPath + File.separator + imageName);
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(takePictureIntent,
+                CAMERA_IMAGE_REQUEST);
     }
 
     public void askSpeechInput(View view) {
@@ -57,10 +85,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("in activity", "succes");
         super.onActivityResult(requestCode, resultCode, data);
-        mImageView = (ImageView) findViewById(R.id.photo);
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
+                Log.d("in switch speech", Integer.toString(RESULT_OK));
                 if (resultCode == RESULT_OK && null != data) {
 
                     ArrayList<String> result = data
@@ -70,14 +99,30 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
 
-            case (REQUEST_IMAGE_CAPTURE): {
-                if (data != null && resultCode == RESULT_OK) {
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-                    mImageView.setImageBitmap(imageBitmap);
-                }
-            }
+            case CAMERA_IMAGE_REQUEST:
 
+                Bitmap bitmap = null;
+                try {
+                    GetImageThumbnail getImageThumbnail = new GetImageThumbnail();
+                    bitmap = getImageThumbnail.getThumbnail(fileUri, this);
+                } catch (FileNotFoundException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+
+                // Setting image image icon on the imageview
+                ImageView imageView = (ImageView) this
+                        .findViewById(R.id.photo);
+                imageView.setImageBitmap(bitmap);
+                break;
+
+            default:
+                Toast.makeText(this, "Something went wrong...",
+                        Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 }
